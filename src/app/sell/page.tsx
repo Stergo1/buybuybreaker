829c2e4db6c5188f95755e6ebee13e57245932c2
Stergo1 <stergosam@gmail.com>
2026-09-21 18:47:08 -0400

    Create page.tsx

---
+++ src/app/sell/page.tsx
@@ -0,0 +1,8 @@
+export default function SellPage() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Sell your equipment</h1>
+      <p>Seller tools will be added next.</p>
+    </div>
+  );
+}
