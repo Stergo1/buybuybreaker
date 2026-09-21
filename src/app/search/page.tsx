a4f8fa68f122e7b77d12f0a8f4eeed6245d3a58b
Stergo1 <stergosam@gmail.com>
2026-09-21 18:45:34 -0400

    Create page.tsx

---
+++ src/app/search/page.tsx
@@ -0,0 +1,8 @@
+export default function SearchPage() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Search</h1>
+      <p>Listings will appear here after we connect the database.</p>
+    </div>
+  );
+}
