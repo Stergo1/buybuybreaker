ee772330c5869286c3b9150efb1525aa339d9abe
Stergo1 <stergosam@gmail.com>
2026-09-24 16:00:37 -0400

    Update page.tsx

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignUp } from "@clerk/nextjs";
+import { Waitlist } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
     <div style={{ minHeight: "100vh", display: "grid", placeItems: "center", background: "#0f2744" }}>
-      <SignUp />
+      <Waitlist />
     </div>
   );
 }
